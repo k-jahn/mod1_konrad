daedd57a8a2aba1a56262c5d3bb0ef7f38013a33
kj <konrad.jahn.84@gmail.com>
2017-11-16 15:47:14 +0100

    finished modal rework with document.onclick

--- scripts/modalMaps.js
+++ scripts/modalMaps.js
@@ -1,97 +1,37 @@
-console.log("modalMaps.js loaded")
 
-//define vars
+// //new structure using document.onclick
 
-var katz = {
-    link: document.getElementById('linkKatz'),
-    modal: document.getElementById('modalKatz')
-};
-var green = {
-    link: document.getElementById('linkGreen'),
-    modal: document.getElementById('modalGreen')
-};
-var yeag = {
-    link: document.getElementById('linkYeag'),
-    modal: document.getElementById('modalYeag')
-};
-var hart = {
-    link: document.getElementById('linkHart'),
-    modal: document.getElementById('modalHart')
-};
-var north = {
-    link: document.getElementById('linkNorth'),
-    modal: document.getElementById('modalNorth')
-};
-var south = {
-    link: document.getElementById('linkSouth'),
-    modal: document.getElementById('modalSouth')
-};
+//close active modal window function
 
-var school = [katz, green, yeag, hart, north, south];
-
-var modalClose = document.getElementsByClassName('modalClose');
-
-closeFunc = function() {
+function closeModal(){
+    console.log('Close Modal Window')
     var active = document.getElementsByClassName('modalActive');
     active[0].className = active[0].className.slice(0, -" modalActive".length);
 }
 
-//open modal on click
-
-// for (var i in school) {
-//     school[i].link.onclick = function() {
-//         school[i].modal.className += " modalActive";
-//     }
-// }
-school[0].link.onclick = function() {
-    school[0].modal.className += " modalActive";
+function openModal(school){
+    console.log("Open Modal Window "+event.target.dataset.school)
+    document.getElementById("modal"+school).className += " modalActive";
+    //document.getElementById("modal"+school).className += " modalActiveBG";
 }
-school[1].link.onclick = function() {
-    school[1].modal.className += " modalActive";
-}
-school[2].link.onclick = function() {
-    school[2].modal.className += " modalActive";
-}
-school[3].link.onclick = function() {
-    school[3].modal.className += " modalActive";
-}
-school[4].link.onclick = function() {
-    school[4].modal.className += " modalActive";
-}
-school[5].link.onclick = function() {
-    school[5].modal.className += " modalActive";
-}
-
-
-
-
-//hide modal when close is clicked
-for (var i in modalClose) {
-    modalClose[i].onclick = function() {
-        closeFunc();
-    }
-}
-//hide modal when clicking outside of content
-
-
-//new structure using document.onclick
-
-
-function modalOn(modal){
-    modal.className +=  " modalActive"
-    
-}
-
 
 document.onclick = function(event){
     console.log(event)
-    // if(event.target.className.indexOf("closeModal") !== -1){
-    //     close .... 
-    // }
 
-    // if(event.target.className.indexOf("openmodal") !== -1){
-    //     open...
-    //     event.target.dataSet.modal =
-    // }
+    //open modal window
+    if(event.target.className.indexOf("modalOpen")!=-1){
+        var school=event.target.dataset.school;
+        openModal(school);
+    }
+
+
+    // close modal window with x button
+    if(event.target.className.indexOf("modalClose")!=-1) {
+        closeModal();
+    }
+    // close modal window on lick on background
+    if(event.target.className.indexOf("modalWrapper")!=-1) {
+        closeModal();
+    }   
 
 }
