3f518cbaefbaf5addae5f0798825024e087c9341
kj <konrad.jahn.84@gmail.com>
2017-11-17 00:34:54 +0100

    fixed modal fades!

--- scripts/modalMaps.js
+++ scripts/modalMaps.js
@@ -4,26 +4,27 @@
 //close active modal window function
 
 function closeModal(){
-    console.log('Close Modal Window')
+    console.log('...and its gone')
     var active = document.getElementsByClassName('modalActive');
     active[0].className = active[0].className.slice(0, -" modalActive".length);
+    document.getElementById("modalShader").className="";
 }
 
 function openModal(school){
-    console.log("Open Modal Window "+event.target.dataset.school)
+    console.log("Look! A Modal Window "+event.target.dataset.school)
+    // give window active class
     document.getElementById("modal"+school).className += " modalActive";
-    //document.getElementById("modal"+school).className += " modalActiveBG";
+    // give shader active class
+    document.getElementById("modalShader").className += " shaderActive";
 }
-
 document.onclick = function(event){
-    console.log(event)
+    //console.log(event)
 
     //open modal window
     if(event.target.className.indexOf("modalOpen")!=-1){
         var school=event.target.dataset.school;
         openModal(school);
     }
-
 
     // close modal window with x button
     if(event.target.className.indexOf("modalClose")!=-1) {
