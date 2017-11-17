51485240f01ff67b801f143b66d0f50f6fe6ac69
kj <konrad.jahn.84@gmail.com>
2017-11-17 12:53:12 +0100

    more working form JS

--- scripts/regForm.js
+++ scripts/regForm.js
@@ -16,6 +16,24 @@ document.onclick = function(event) {
             // show sizes
             document.getElementById('uniformSize').classList.remove("inactive");
         }
-
     }
+
+    // if first school is clicked
+    if (event.target.name == 'school1') {
+        console.log('you clicked'+event.target.id)
+        //remove inactive class from all second schools
+        var schools=document.getElementsByClassName('school2label');
+        for(var x of schools) {
+            x.classList.remove('inactive');
+        }
+        // id of second school to block
+        var blockId=event.target.id.substr(0,event.target.id.length-1)+2;
+        
+        //remove check from second school if present
+        console.log(document.getElementById(blockId))
+        document.getElementById(blockId).checked=false;
+        //add inactive class to second school
+        document.getElementById(blockId+'label').classList.add('inactive')
+    }
+    // 
 }
