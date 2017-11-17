de5fbd0e1516f9d2b85493c473fb7e0bd5baa748
kj <konrad.jahn.84@gmail.com>
2017-11-17 08:14:37 +0100

    radio buttons and small fixes

--- scripts/modalMaps.js
+++ scripts/modalMaps.js
@@ -1,38 +1,40 @@
+// modal Window JS for Game Location Maps
 
-// //new structure using document.onclick
-
+// open modal window function
+function openModal(school) {
+    console.log("Look! A Modal Window for " + event.target.dataset.school)
+    // give window active class
+    document.getElementById("modal" + school).className += " active";
+    // give shader active class
+    document.getElementById("modalShader").className += " active";
+}
 //close active modal window function
 
-function closeModal(){
+function closeModal() {
     console.log('...and its gone')
-    var active = document.getElementsByClassName('modalActive');
-    active[0].className = active[0].className.slice(0, -" modalActive".length);
-    document.getElementById("modalShader").className="";
+    // remove active window class
+    var active = document.getElementsByClassName('modalWrapper active');
+    active[0].className = active[0].className.slice(0, -" active".length);
+    // remove active shader class
+    document.getElementById("modalShader").className = "";
 }
-
-function openModal(school){
-    console.log("Look! A Modal Window "+event.target.dataset.school)
-    // give window active class
-    document.getElementById("modal"+school).className += " modalActive";
-    // give shader active class
-    document.getElementById("modalShader").className += " shaderActive";
-}
-document.onclick = function(event){
+// click listener function
+document.onclick = function(event) {
     //console.log(event)
-
+    console.log('click');
     //open modal window
-    if(event.target.className.indexOf("modalOpen")!=-1){
-        var school=event.target.dataset.school;
+    if (event.target.className.indexOf("modalOpen") != -1) {
+        var school = event.target.dataset.school;
         openModal(school);
     }
 
     // close modal window with x button
-    if(event.target.className.indexOf("modalClose")!=-1) {
+    if (event.target.className.indexOf("modalClose") != -1) {
         closeModal();
     }
     // close modal window on lick on background
-    if(event.target.className.indexOf("modalWrapper")!=-1) {
+    if (event.target.className.indexOf("modalWrapper") != -1) {
         closeModal();
-    }   
+    }
 
 }
